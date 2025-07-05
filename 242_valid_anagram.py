26bc637d384181b5c3e016d501a839e40d7d6ae5
Ranjit Kumar Mahto <ranjit20013kumar@gmail.com>
2025-07-05 23:16:17 +0530

    Update 242_valid_anagram.py

--- 242_valid_anagram.py
+++ 242_valid_anagram.py
@@ -10,3 +10,23 @@ class Solution:
         if all(value == 0 for value in freq.values()):
             return True
         return False
+
+
+#Approach 2
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+
+        countS, countT = {}, {}    
+
+        for i in range(len(s)):
+            countS[s[i]] = 1+countS.get(s[i],0)
+            countT[t[i]] = 1+countT.get(t[i],0)
+
+        for c in countS:
+            if countS[c]!=countT.get(c,0):
+                return False
+        return True 
+        
